5fdb5d671041dd818a8b5f40abd3c8c119a4e16b
mohammad sahrayi <mohammadsahrayi90@gmail.com>
2022-09-27 09:13:54 +0330

    ProductCategoryImpl

---
+++ LampShade/01.Framwork/Domain/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Framwork.Domain
+{
+   public class EntityBase
+    {
+        public long ID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public EntityBase()
+        {
+            CreationDate = DateTime.Now;
+        }
+    }
+}
